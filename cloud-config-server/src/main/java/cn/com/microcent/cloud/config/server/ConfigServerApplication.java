cfb8d1002f753f04a443d7d984afc2429e1883f5
microcent <1287994577@qq.com>
2017-03-27 15:15:19 +0800

    Initial commit

---
+++ cloud-config-server/src/main/java/cn/com/microcent/cloud/config/server/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package cn.com.microcent.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by Administrator on 2017/3/27.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
